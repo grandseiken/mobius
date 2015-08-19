2274c8aa30ff10501d8536b926d05cc24c4b5822
stu <s+github@stu.scot>
2015-08-19 22:32:07 +0100

    Grain layer tweaks.

--- src/shaders/grain.fragment.glsl
+++ src/shaders/grain.fragment.glsl
@@ -5,17 +5,21 @@ out vec4 output_colour;
 uniform float amount;
 uniform float frame;
 
+float simplex_layer(vec3 seed, float time, float pow)
+{
+  return simplex3(seed / pow - vec3(0., 0., time / pow)) / pow;
+}
+
 void main()
 {
-  float time = frame / 32.;
-  vec3 seed = vec3(gl_FragCoord.xy / 4., 0.);
+  float time = frame / 16.;
+  vec3 seed = vec3(gl_FragCoord.xy / 2., gl_FragCoord.x / 8.);
 
-  float n = 0.;
-  n += simplex3(seed - vec3(0., 0., time / 3.375));
-  n += 2. * simplex3(2. * seed - vec3(0., 0., time / 2.25));
-  n += 4. * simplex3(4. * seed - vec3(0., 0., time / 1.5));
-  n /= 4.;
+  float n =
+      simplex_layer(seed, time, 1.) +
+      simplex_layer(seed, time, 2.) +
+      simplex_layer(seed, time, 4.);
   float v = clamp((n + 1.) / 2., 0., 1.);
-  output_colour = vec4(v, v, v, amount);
+  output_colour = vec4(v, v, v, v * amount);
 }
 
