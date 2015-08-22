fe22cb467234bb331696fa8cf19e3db74d353369
stu <s+github@stu.scot>
2015-08-22 17:15:50 +0100

    Gamma correction.

--- src/shaders/main.fragment.glsl
+++ src/shaders/main.fragment.glsl
@@ -7,6 +7,10 @@ out vec4 output_colour;
 uniform vec3 light_source;
 uniform float light_intensity;
 
+const float gamma = 2.2;
+const vec3 gamma_uncorrect = vec3(gamma);
+const vec3 gamma_correct = vec3(1. / gamma);
+
 void main()
 {
   vec3 light_difference = light_source - vertex_world;
@@ -19,6 +23,8 @@ void main()
   const float ambient = .1;
   intensity = clamp(ambient + intensity, 0., 1.);
 
-  output_colour = vec4(intensity * vertex_colour, 1.);
+  vec3 lit_colour =
+      pow(intensity * pow(vertex_colour, gamma_uncorrect), gamma_correct);
+  output_colour = vec4(lit_colour, 1.);
 }
 
